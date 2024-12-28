f7920a8500393bd37f0dbdfe8d9cae2609fe920a
00752-tech <passiveincomestreaming@gmail.com>
2024-12-28 14:32:51 -0800

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { HeroSection } from '@/components/hero-section'
+import { BenefitsSection } from '@/components/benefits-section'
+// Import other components as needed
+
+export default function Home() {
+  return (
+    <div>
+      <HeroSection />
+      <BenefitsSection />
+      {/* Add other components here */}
+    </div>
+  )
+}
